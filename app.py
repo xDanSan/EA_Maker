7415642ffbb3012e12945bfb31ee2f5dfdb975e8
Mohamed Issa <30409811+xDanSan@users.noreply.github.com>
2023-03-06 08:19:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,35 @@
 import os
-import streamlit as st
 import subprocess
+import streamlit as st
 
-# Set execute permission on the metaeditor.exe file
-os.chmod("metaeditor.exe", 0o755)
+# Check if Wine is already installed
+wine_check = subprocess.run(['which', 'wine'], capture_output=True)
+if wine_check.returncode != 0:
+    # Wine is not installed, so install it
+    subprocess.run(['sudo', 'dpkg', '--add-architecture', 'i386'], check=True)
+    subprocess.run(['wget', '-nc', 'https://dl.winehq.org/wine-builds/winehq.key'], check=True)
+    subprocess.run(['sudo', 'apt-key', 'add', 'winehq.key'], check=True)
+    subprocess.run(['sudo', 'add-apt-repository', 'deb', 'https://dl.winehq.org/wine-builds/ubuntu/', 'focal', 'main'], check=True)
+    subprocess.run(['sudo', 'apt-get', 'update'], check=True)
+    subprocess.run(['sudo', 'apt-get', '-y', 'install', 'winehq-stable'], check=True)
 
-# Define the path to the metaeditor.exe file
-metaeditor_path = os.path.join(os.getcwd(), "metaeditor.exe")
+# Define the path to the MetaEditor application
+metaeditor_path = os.path.join(os.getcwd(), 'metaeditor.exe')
 
-# Define the function to compile the MQ4 file
-def compile_mq4(mq4_file_path):
-    # Define the path to the output EX4 file
-    ex4_file_path = os.path.splitext(mq4_file_path)[0] + ".ex4"
+# Set the Wine display environment variable
+os.environ['DISPLAY'] = ':0'
 
-    # Call the metaeditor.exe file using Wine to compile the MQ4 file
-    process = subprocess.Popen(["wine", metaeditor_path, "/compile:%s" % mq4_file_path])
-    process.wait()
+# Create a file uploader widget
+mq4_file = st.file_uploader('Upload your MQ4 file', type='mq4')
 
-    # Check if the EX4 file was created
-    if not os.path.exists(ex4_file_path):
-        st.error("Compilation failed. Could not create the EX4 file.")
-    else:
-        st.success("Compilation successful. Download the EX4 file below.")
-        # Add a download link for the compiled EX4 file
-        with open(ex4_file_path, "rb") as ex4_file:
-            ex4_file_contents = ex4_file.read()
-            st.download_button("Download compiled EX4 file", ex4_file_contents, "compiled_file.ex4")
+if mq4_file is not None:
+    # Save the uploaded file to disk
+    with open('uploaded_file.mq4', 'wb') as f:
+        f.write(mq4_file.read())
 
-# Define the Streamlit app
-def app():
-    st.title("MQ4 Compiler")
+    # Compile the uploaded file using MetaEditor and Wine
+    cmd = ['wine', metaeditor_path, '/compile', 'uploaded_file.mq4']
+    subprocess.run(cmd, check=True)
 
-    # Add a file upload widget
-    mq4_file = st.file_uploader("Upload your MQ4 file", type="mq4")
-
-    # Check if a file was uploaded
-    if mq4_file is not None:
-        # Save the MQ4 file to a temporary location
-        with open("temp.mq4", "wb") as temp_file:
-            temp_file.write(mq4_file.getvalue())
-
-        # Compile the MQ4 file
-        compile_mq4("temp.mq4")
-
-# Run the app
-if __name__ == "__main__":
-    app()
+    # Display a message when the file is compiled
+    st.success('Compilation successful!')
